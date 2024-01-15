076a3bc0d163d690b1a6091a26dab8fa65bc9de5
adaozer <115459409+adaozer@users.noreply.github.com>
2024-01-15 21:00:42 +0000

    exceptions handled?

--- finish.cpp
+++ finish.cpp
@@ -1,4 +1,5 @@
 #include "finish.h"
+#include <stdexcept>
 
 double find_length(Vector u) {  // Function finding the length of a vector u
     double result = 0;  // Initialise result variable to zero to be returned
@@ -21,17 +22,32 @@ Basis arg_to_param(int argc, char* argv[]) {  // argument to parameter function
     string arg = argv[i];
     if (arg[0] == '[') {  // Check if the argument shows the start of a vector
         arg.erase(remove(arg.begin(), arg.end(), '['), arg.end());  // Remove [
+        try {
         double argf = stof(arg);  // Turn the string arg into a float
         vec.push_back(argf);  // Push  number value to placeholder vector.
+        } catch (exception& e) {  // Catch exceptions
+            Basis fail;
+            return fail;  // Return empty basis
+        }
     } else if (arg.back() == ']') {  // Check if argument shows end of a vector
         arg.erase(remove(arg.begin(), arg.end(), ']'), arg.end());  // Remove ]
+        try {
         double argf = stof(arg);  // Turn the string arg into a float
         vec.push_back(argf);  // Push the number to the placeholder vector
         basis.push_back(vec);  // Push the placeholder vector to the basis
         vec.clear();  // Empty placeholder vector for next loop iteration
+        } catch (exception& e) {  // Catch exceptions
+            Basis fail;
+            return fail;  // Return empty basis
+        }
     } else {  // If arg doesn't have [ or ], simply add arg to vec
+        try {
         double argf = stof(arg);
         vec.push_back(argf);
+        } catch (exception& e) {  // Catch exceptions
+            Basis fail;
+            return fail;  // Return empty basis
+        }
     }
     }
     return basis;  // Return the basis to be used as a parameter
